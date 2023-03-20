be491bbe476566c44c90bc078b4bdc52b7e896f2
vincenttommi <vincenttommi@gmail.com>
2023-03-20 22:11:30 +0300

    foreach() in js

---
+++ foreach.js
@@ -0,0 +1,11 @@
+// Array.prototype.forEach()
+//foreach method excutes a provided function  for each array element
+
+const array1 = ["a", "b","c"];
+
+array1.forEach(element => console.log(element));
+
+
+// The forEach() method is an iterative method. It calls a
+// provided callbackFn function once for each element in an array in ascending-index order. Unlike map(), forEach() always returns undefined and is not chainable. The typical use 
+//case is to execute side effects at the end of a chain.
